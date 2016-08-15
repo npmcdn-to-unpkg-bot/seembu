eaef154e3a76321519b4fc3ed2a8f43a1f1eec8c
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2016-08-15 15:32:24 -0400

    Setting up configs for email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+
+	default from: "services@seembu.com"
+
+	  def welcome_email(user)
+	  	@user = user
+	  	mail(to: @user.email, subject: 'Welcome to Codeplace')
+	  end
+
 end
