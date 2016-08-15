d37444d4ccc68e41a01922dac8265456bcd15600
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2016-08-15 15:10:23 -0400

    Adjusting presume project latest

--- app/controllers/prehome_controller.rb
+++ app/controllers/prehome_controller.rb
@@ -1,7 +1,7 @@
 class PrehomeController < ApplicationController
 
   def index
-  	@projects = Project.all.paginate(:page => params[:page], :per_page => 9)
+  	@projects = Project.order("created_at DESC").paginate(:page => params[:page], :per_page => 6)
   end
 
 end
